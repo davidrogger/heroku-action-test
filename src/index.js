c8c1fb6c4c5a82608067eef32b9862611d960b45
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-11 18:57:26 -0300

    initial commit deploy heroku

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send(`<h1> Runing at port: ${PORT}`));
+
+app.listen(PORT, () => console.log(`Online: ${PORT}`));
